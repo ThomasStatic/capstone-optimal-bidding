f754d9ed0ea0f9b453d2d1020d60aeda7b0de3d7
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2025-11-12 13:03:08 -0500

    Creating method for preparing EIA data for statsmodel

--- shell/load_sarimax_projections.py
+++ shell/load_sarimax_projections.py
@@ -1,7 +1,19 @@
+from typing import Any
+from pandas import DataFrame, Series
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 class SARIMAXLoadProjections:
-    def __init__(self, historic_data):
-        self.historic_data = historic_data
+    _historic_data: DataFrame
+    _y: Series[Any]
 
-    
+    def __init__(self, historic_data: DataFrame):
+        self._historic_data = historic_data
+
+    def _prepare_historical_data(self):
+        '''Prepare historical load data for SARIMAX modeling.'''
+        # Convert 'period' to UTC and remove timezone info
+        self._y = (self._historic_data.assign(period=lambda x: x['period'].dt.tz_convert("UTC").dt.tz_localize(None))
+                .set_index('period')["load_MWH"].asfreq('H'))
+        
+        # Fill in missing time periods by interpolating values
+        self._y = self._y.interpolate(limit_direction='both')
