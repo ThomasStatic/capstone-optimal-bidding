50c3b0fb335964896a20d3e56af20cb42df7f31f
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2025-11-12 12:22:35 -0500

    refining main calls to class functions

--- shell/main.py
+++ shell/main.py
@@ -1,8 +1,8 @@
 from enverus_api import EnverusAPI
 from market_loads_api import ISODemandController
 
-#api = EnverusAPI()
-#api.generate_token()
+api = EnverusAPI()
+api.generate_token()
 
-marketDemandController = ISODemandController("2023-01-01", "2023-01-31")
-marketDemandController.get_market_loads()
+marketDemandController = ISODemandController("2023-01-01", "2023-01-31", "ERCOT")
+print(marketDemandController.get_market_loads().head())
