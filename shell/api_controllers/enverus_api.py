52e52b087fdda3f52e9731621ce81982c651f9c8
Thomas Boyle <tsboyle10@gmail.com>
2026-01-27 12:30:09 -0500

    renaming folder for imports

--- shell/api_controllers/enverus_api.py
+++ shell/api_controllers/enverus_api.py
@@ -25,5 +25,19 @@ class EnverusAPI:
 
         if not self._token:
             raise ValueError("Failed to retrieve token from response")
+    
+    def get_power_production_data(self) -> None:
+        '''Fetches data required for calculating marginal cost.'''
+        if not self._token:
+            raise ValueError("API token is not generated. Call generate_token() first.")
         
+        url = f"{self.base_url}/plant-level"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self._token}"
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        print(data)  # For demonstration purposes, print the data
     
